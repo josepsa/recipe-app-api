6f33da1e8ddfcec0ecbda5e476a0cd4ae82d1205
josepsa <sajanjose.withu@gmail.com>
2022-12-06 10:02:09 +0300

    added admin customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    '''Define admin pages for user'''
+    ordering=['id']
+    list_display=['email','name']
+
+#'UserAdmin' should mention to take the changes done above
+admin.site.register(models.User,UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,4 +6,32 @@ from django.test import Client
 
 class AdminTests(TestCase):
     '''Test for django-admin'''
-    
+    #'setUp' name should be same, else it will not work
+    def setUp(self):
+        self.client=Client()
+        self.admin_user=get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='samplepassword123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user=get_user_model().objects.create_user(
+            email='user@example.com',
+            password='samplepassword123',
+            name='Test User'
+        )
+
+    def test_users_list(self):
+        '''test users are listed on django-admin page'''
+        # it will pull below url, it is django built in url
+        url=reverse('admin:core_user_changelist')
+        res=self.client.get(url)
+
+        self.assertContains(res,self.user.name)
+        self.assertContains(res,self.user.email)
+
+    def test_edit_user(self):
+        url=reverse('admin:core_user_change',args=[self.user.id])
+        res=self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
+        
